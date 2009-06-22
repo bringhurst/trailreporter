ec629324e0ab8f1a16debac000999f1ca01708c0
Jon Bringhurst <jon@bringhurst.org>
2009-06-22 02:00:16 +0000

    a ton of changes and a django library change

--- trailreporter/incident/bforms.py
+++
@@ -1,9 +0,0 @@
-from django import newforms as forms
-import models
-from google.appengine.ext.db import djangoforms
-
-class IncidentForm(djangoforms.ModelForm):
-    class Meta:
-        model = models.Incident
-        exclude = ['created_by']
-
