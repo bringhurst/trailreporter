62e2701bd7174586b8a5b358c422647012c453da
Jon Bringhurst <jon@bringhurst.org>
2009-06-22 03:31:07 +0000

    foo

--- tags/trailreporter-000001/incident/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from incident.models import Incident
-
-class IncidentAdmin(admin.ModelAdmin):
-    list_display = ('short_summary','date')
-
-admin.site.register(Incident, IncidentAdmin)
